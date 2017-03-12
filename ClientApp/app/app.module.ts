84ad0b8b2f2b4e6d5953a6b126411cd80d136848
borkovski <borkovski@o2.pl>
2017-03-12 01:44:15 +0100

    Transfer edit client form

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -4,25 +4,25 @@ import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        TransferComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'transfer', component: TransferComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ ClientApp/app/components/transfer/transfer.component.ts
@@ -0,0 +1,44 @@
+﻿import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'transfer',
+    template: require('./transfer.component.html')
+})
+export class TransferComponent {
+    myForm: FormGroup;
+    submitted: boolean = false;
+
+    constructor(fb: FormBuilder) {
+        this.myForm = fb.group({
+            'title': [null, Validators.required],
+            'amount': [null],
+            'direction': [null],
+            'date': [null],
+            'repeat': [null],
+            'repeatPeriod': [null],
+            'repeatMonth': [null],
+            'repeatDay': [null],
+            'isContinuous': [null]
+        });
+        this.myForm.controls['repeat'].valueChanges.subscribe((value: boolean) => {
+            if (!value) {
+                this.myForm.controls['repeatPeriod'].setValue(null);
+                this.myForm.controls['repeatMonth'].setValue(null);
+                this.myForm.controls['repeatDay'].setValue(null);
+            }
+        });
+        this.myForm.controls['repeatPeriod'].valueChanges.subscribe((value: string) => {
+            if (value == '3') {
+                this.myForm.controls['repeatMonth'].setValue(null);
+            }
+        });
+    }
+
+    onSubmit(value: any): void {
+        this.submitted = true;
+        if (this.myForm.valid) {
+            console.log(value);
+        }
+    }
+}
